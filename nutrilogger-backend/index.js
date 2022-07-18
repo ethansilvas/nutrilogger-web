c4019293e397ae526f439e7448d883e251d0c400
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-17 19:41:15 -0700

    connect to server and start env

--- nutrilogger-backend/index.js
+++ nutrilogger-backend/index.js
@@ -2,7 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import 'dotenv/config';
 
+const app = express(); 
 
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nutricluster.p53q4ya.mongodb.net/?retryWrites=true&w=majority`;  //'mongodb+srv://esdev:hM26Z6iWo9SP@nutricluster.p53q4ya.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
 
+//mongoose.connect(CONNECTION_URL).then(() => {console.log('started')});
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));

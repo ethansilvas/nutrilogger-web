ee93eeebb035fc1c5164afe2a452d6433c611afd
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-18 18:16:42 -0700

    start css

--- nutrilogger-frontend/src/components/Home/Home.js
+++ nutrilogger-frontend/src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
 import FoodSearch from "../FoodSearch/FoodSearch";
-import { Container } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
 import MainNav from "../MainNav/MainNav";
 
 const Home = () => {
     return (
         <Container maxWidth='lg'>
             <MainNav></MainNav>
+            <Grow in>
+                <Container>
+                    <Grid container spacing={2} justifyContent='space-between'>
+                        <Grid item xs={12}><FoodSearch></FoodSearch></Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
--- nutrilogger-frontend/src/components/MainNav/MainNav.js
+++ nutrilogger-frontend/src/components/MainNav/MainNav.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core"; 
+import { AppBar, Typography } from "@material-ui/core";
+
+import useStyles from './styles';
 
 const MainNav = () => {
+    const classes = useStyles();
+
     return (
-        <AppBar position='static'>
+        <AppBar position='static' className={classes.mainNav}>
             <Typography variant='h4' align='center'>Nutrilogger</Typography>
         </AppBar>
     );

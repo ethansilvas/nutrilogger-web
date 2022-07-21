28945089b975122915814ea605f4bb5cba5ff634
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-20 20:48:32 -0700

    setup react router

--- nutrilogger-frontend/src/App.js
+++ nutrilogger-frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'; 
+import { Container } from '@material-ui/core';
+
 import Home from './components/Home/Home';
+import MainNav from "./components/MainNav/MainNav";
 
 const App = () => {
     return (
-        <Home></Home>
+        <BrowserRouter>
+            <Container maxWidth='lg'>
+                <MainNav />
+                <Switch>
+                    <Route path='/' exact component={() => <Redirect to='/search' />} />
+                    <Route path='/search' exact component={Home} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     );
 }
 
--- nutrilogger-frontend/src/components/FoodSearch/FoodSearch.js
+++ nutrilogger-frontend/src/components/FoodSearch/FoodSearch.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import { getFood } from '../../actions/search';
 import useStyles from './styles';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 const FoodSearch = () => {
     const classes = useStyles();
@@ -11,17 +16,26 @@ const FoodSearch = () => {
     const dispatch = useDispatch();
     const food = useSelector((state) => state.food);
 
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+
     console.log(food);
 
     useEffect(() => {
         dispatch(getFood());
     }, [dispatch]);
 
+    const handleSubmit = () => {
+
+    }
+
     return (
         <Paper className={classes.paper}>
-            <form className={classes.foodSearchForm} autoComplete='off' noValidate >
+            <form className={classes.foodSearchForm} autoComplete='off' noValidate onSubmit={handleSubmit}>
                 <Typography variant='h5'>Search for a food:</Typography>
-                <TextField name='food' variant='outlined' label='"Banana", "McDonalds", ...' fullWidth></TextField>
+                <TextField name='search' variant='outlined' label='Search for a food, brand, restaurant, etc.' fullWidth value='food' onChange={() => {}} />
             </form>
         </Paper>
     );

7b9d5c5e6b01fd786252a5076c1c757f2cc0c8a7
Ethan Silvas <ethansilvas2@gmail.com>
2022-07-22 13:40:56 -0700

    set no search term to search all

--- nutrilogger-frontend/src/api/index.js
+++ nutrilogger-frontend/src/api/index.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:5000/search' });
 
-export const getFood = (searchQuery) => API.get(`/search?searchQuery=${searchQuery.search || 'none'}`);
+export const getFood = (searchQuery) => API.get(`/search?searchQuery=${searchQuery ? searchQuery.search : ''}`);
